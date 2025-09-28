cbc428f5cdd01f02669e62076ab7a6c015b23e05
Manojrakp <manojrakp@gmail.com>
2025-09-28 22:15:29 +0530

    creating first table & insering the values

---
+++ src/main/java/com/ecom/entity/User.java
@@ -0,0 +1,27 @@
+package com.ecom.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+@Entity
+@Table(name = "ECOMUSERS")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @SequenceGenerator(name = "user_seq", sequenceName = "USER_SEQ", allocationSize = 1)
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+}
+
