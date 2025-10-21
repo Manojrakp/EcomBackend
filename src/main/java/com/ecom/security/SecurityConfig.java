1de654226809a357663d88a6860bb7b244dfaccc
Manojrakp <manojrakp@gmail.com>
2025-10-21 16:36:45 +0530

    feat: spring Security & Authentication

---
+++ src/main/java/com/ecom/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.ecom.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+//@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/public/**").permitAll() // Allow public access to certain paths
+                        .anyRequest().authenticated() // Require authentication for all other requests
+                )
+                .formLogin(form -> form
+                        .loginPage("/login") // Specify custom login page
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .permitAll());
+        return http.build();
+    }
+}
